a9a69bee152c0b9cf70d12c26b037dc824161431
amine omari <omari1amineco@gmail.com>
2025-01-22 14:16:43 +0300

    feat: import MainCOntainer to layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,11 @@
-import { Slot, SplashScreen } from "expo-router/build/exports";
+import { SplashScreen } from "expo-router/build/exports";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 import "../global.css";
+import MainContainer from "./MainContainer";
 
-export default Slot;
+export default function RootLayout() {
+  return <MainContainer />;
+}
