1ed943000225ff5bab426d40b9179b09fa49dc59
amine omari <omari1amineco@gmail.com>
2025-01-22 14:16:21 +0300

    feat: add new MainContainer component

---
+++ app/MainContainer.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const MainContainer = () => {
+  return (
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+
+export default MainContainer;
