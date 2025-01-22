03de461b7132ee64a7d2f2d8e94ca3fe6f743d3e
amine omari <omari1amineco@gmail.com>
2025-01-22 14:04:28 +0300

    feat: tailwind installation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,6 @@ import { Slot, SplashScreen } from "expo-router/build/exports";
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
-  return <Slot />;
-}
+import "../global.css";
+
+export default Slot;
